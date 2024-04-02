abe00ef0737e328b1200dac6f630b5f637727acb
Jonathan Stiven Colorado Largo <jecl6386@hotmail.com>
2024-04-02 17:13:38 -0500

    Set Dinamic Server For Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Config;
-
 /**
  * @OA\OpenApi(
  *     @OA\Info(
- *         title="Mi System Laragpt",
+ *         title="System Laragpt",
  *         version="1.0",
  *         description="Descripción de mi API"
  *     ),
- *     @OA\Server(url=LaravelConstant::API_URL)
+ *     @OA\Server(
+*      url = L5_SWAGGER_CONST_HOST,
+ *     description = "My Server",
+ *    )
  * )
  */
 class Controller extends BaseController
